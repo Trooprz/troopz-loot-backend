3b60925608ca38e38b2b3c6f944753a51c0d1f76
christophmarckx <christoph.marckx@cegeka.com>
2022-07-26 16:24:09 +0200

    first commit

---
+++ src/main/java/com/troopz/trpzlootbackend/TrpzLootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.troopz.trpzlootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrpzLootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrpzLootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/troopz/trpzlootbackend/TrpzLootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troopz.trpzlootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrpzLootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
